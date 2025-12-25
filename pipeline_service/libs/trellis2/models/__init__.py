70b76dea5159a37a317905eb00f3b19b9befd76f
root <root@goldtooth.inc>
2025-12-25 18:40:16 +0200

    feat: updatE

--- pipeline_service/libs/trellis2/models/__init__.py
+++ pipeline_service/libs/trellis2/models/__init__.py
@@ -10,6 +10,10 @@ __attributes = {
     'SLatFlowModel': 'structured_latent_flow',
     'ElasticSLatFlowModel': 'structured_latent_flow',
     
+    # SLat VAE (from trellis v1 for gaussian output)
+    'SLatEncoder': 'structured_latent_vae',
+    'SLatGaussianDecoder': 'structured_latent_vae',
+    
     # SC-VAEs
     'SparseUnetVaeEncoder': 'sc_vaes.sparse_unet_vae',
     'SparseUnetVaeDecoder': 'sc_vaes.sparse_unet_vae',
@@ -73,6 +77,7 @@ if __name__ == '__main__':
     from .sparse_structure_vae import SparseStructureEncoder, SparseStructureDecoder
     from .sparse_structure_flow import SparseStructureFlowModel
     from .structured_latent_flow import SLatFlowModel, ElasticSLatFlowModel
+    from .structured_latent_vae import SLatEncoder, SLatGaussianDecoder
         
     from .sc_vaes.sparse_unet_vae import SparseUnetVaeEncoder, SparseUnetVaeDecoder
     from .sc_vaes.fdg_vae import FlexiDualGridVaeEncoder, FlexiDualGridVaeDecoder
